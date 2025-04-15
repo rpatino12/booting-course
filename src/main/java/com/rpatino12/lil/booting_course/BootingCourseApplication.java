53cb99d2751286c5ccf0dd61c225ed0dde4b86d5
Ricardo Patino <rpatino12@outlook.com>
2025-04-15 15:57:00 -0500

    First commit

---
+++ src/main/java/com/rpatino12/lil/booting_course/BootingCourseApplication.java
@@ -0,0 +1,13 @@
+package com.rpatino12.lil.booting_course;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootingCourseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BootingCourseApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/rpatino12/lil/booting_course/BootingCourseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rpatino12.lil.booting_course;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BootingCourseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
